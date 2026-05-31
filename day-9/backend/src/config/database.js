b6850342b4338f2685da7e43551fb26eeeaadfb4
atul kumar <atulkumar879185@gmail.com>
2026-05-31 22:32:21 +0530

    database update

--- day-9/backend/src/config/database.js
+++ day-9/backend/src/config/database.js
@@ -1,10 +1,14 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const dns=require('dns')
 require('dotenv').config();
+dns.setServers(["1.1.1.1","8.8.8.8"]);
 const connectdb = async () => {
-    
-        await mongoose.connect(process.env.mongo_uri);
 
-        console.log('server connect to db');
-    
+  await mongoose.connect(process.env.MONGO_URI);
+
+  console.log('server connect to db');
+
 };
-module.exports=connectdb;
+module.exports = connectdb;
+
+
