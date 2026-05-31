239714840f217e0c760c65e6562de4dc5af7d1ea
atul kumar <atulkumar879185@gmail.com>
2026-05-31 15:29:17 +0530

    URI

--- day-9/backend/src/app.js
+++ day-9/backend/src/app.js
@@ -5,7 +5,7 @@ const path = require('path')
 const cors = require('cors')
 app.use(express.json());
 app.use(cors())
-app.use(express.static('./public'))
+app.use(express.static('./public/Assets'))
 
 
 
