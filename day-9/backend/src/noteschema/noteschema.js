a06ec84c18cb9b1ad48984452d4159ec0bd0e550
atul kumar <atulkumar879185@gmail.com>
2026-05-31 16:02:33 +0530

    Remove day-9 from GitHub

--- day-9/backend/src/noteschema/noteschema.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-const noteschema=new mongoose.Schema({
-    name:String,
-    course:String
-})
-const note=mongoose.model("notedetail",noteschema)
-module.exports=note
